25338af57a7e415e4b8d4a629568e888aa99254c
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-01-07 10:40:39 -0500

    Initial Commit

---
+++ RichTextEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RichTextEditor
+//
+//  Created by Anar Enhsaihan on 1/7/15.
+//  Copyright (c) 2015 Citta LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RichTextEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RichTextEditor
+//
+//  Created by Anar Enhsaihan on 1/7/15.
+//  Copyright (c) 2015 Citta LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
