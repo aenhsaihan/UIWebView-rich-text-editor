bb31ba118ed5e3d7447e155f7d27791c69931857
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-01-07 13:47:01 -0500

    Rich Text Editing: Highlighting and UIMenuController

--- RichTextEditor/ViewController.m
+++ RichTextEditor/ViewController.m
@@ -21,24 +21,30 @@
     NSURL *indexFileURL = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
     [self.webView loadRequest:[NSURLRequest requestWithURL:indexFileURL]];
     
-//    NSMutableArray *items = [[NSMutableArray alloc] init];
-//    
-//    UIBarButtonItem *bold = [[UIBarButtonItem alloc] initWithTitle:@"B" style:UIBarButtonItemStyleBordered target:self action:@selector(bold)];
-//    UIBarButtonItem *italic = [[UIBarButtonItem alloc] initWithTitle:@"I" style:UIBarButtonItemStyleBordered target:self action:@selector(italic)];
-//    UIBarButtonItem *underline = [[UIBarButtonItem alloc] initWithTitle:@"U" style:UIBarButtonItemStyleBordered target:self action:@selector(underline)];
-//    
-//    [items addObjectsFromArray:@[bold, italic, underline]];
-//    
-//    self.navigationItem.rightBarButtonItems = items;
-    
     [self checkSelection:self];
     
     self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(checkSelection:) userInfo:nil repeats:YES];
+    
+    UIMenuItem *highlightMenuItem = [[UIMenuItem alloc] initWithTitle:@"Highlight" action:@selector(highlight)];
+    [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObject:highlightMenuItem]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/*
+ This is a bit more complicated compared to our other actions as we have to check the current color of the selected text to see whether or not to highlight it or to remove the highlight. Firstly we get the current value of the background color, we then check to see if it’s equal to ‘rgb(255, 255, 0)’ which is the rgb equivalent of yellow and if so we reset the color to white else we highlight the selection by setting the background color to yellow.
+ */
+-(void)highlight
+{
+    NSString *currentColor = [self.webView stringByEvaluatingJavaScriptFromString:@"document.queryCommandValue('backColor')"];
+    if ([currentColor isEqualToString:@"rgb(255, 255, 0)"]) {
+        [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand('backColor', false, 'white')"];
+    } else {
+        [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand('backColor', false, 'yellow')"];
+    }
 }
 
 -(void)checkSelection:(id)sender
@@ -55,12 +61,21 @@
     
     [items addObjectsFromArray:@[bold, italic, underline]];
     
+    /*
+     We add the bar button items to the array and then if there are any changes to the status of bold, italic or underline since we last checked or if this is the first time then we set that array as the navigation items right bar button items and update the statuses.
+     */
+    
     if (self.currentBoldStatus != boldEnabled || self.currentItalicStatus != italicEnabled || self.currentUnderlineStatus != underlineEnabled || sender == self) {
         self.navigationItem.rightBarButtonItems = items;
         self.currentBoldStatus = boldEnabled;
         self.currentItalicStatus = italicEnabled;
         self.currentUnderlineStatus = underlineEnabled;
     }
+    
+    NSString *currentColor = [self.webView stringByEvaluatingJavaScriptFromString:@"document.queryCommandValue('backColor')"];
+    BOOL isYellow = [currentColor isEqualToString:@"rgb(255, 255, 0)"];
+    UIMenuItem *highlightMenuItem = [[UIMenuItem alloc] initWithTitle:(isYellow) ? @"De-Highlight" : @"Highlight" action:@selector(highlight)];
+    [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObject:highlightMenuItem]];
 }
 
 -(void)bold {
