ba69ca144c0f0f9748a585c9b27f4623622d889e
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-01-07 11:16:01 -0500

    Create rich text editor using UIWebView with three controls: bold, italic, and underline.

--- RichTextEditor/ViewController.m
+++ RichTextEditor/ViewController.m
@@ -17,11 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *indexFileURL = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:indexFileURL]];
+    
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    
+    UIBarButtonItem *bold = [[UIBarButtonItem alloc] initWithTitle:@"B" style:UIBarButtonItemStyleBordered target:self action:@selector(bold)];
+    UIBarButtonItem *italic = [[UIBarButtonItem alloc] initWithTitle:@"I" style:UIBarButtonItemStyleBordered target:self action:@selector(italic)];
+    UIBarButtonItem *underline = [[UIBarButtonItem alloc] initWithTitle:@"U" style:UIBarButtonItemStyleBordered target:self action:@selector(underline)];
+    
+    [items addObjectsFromArray:@[bold, italic, underline]];
+    
+    self.navigationItem.rightBarButtonItems = items;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)bold {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Bold\")"];
+}
+
+-(void)italic {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Italic\")"];
+}
+
+-(void)underline {
+    [self.webView stringByEvaluatingJavaScriptFromString:@"document.execCommand(\"Underline\")"];
 }
 
 @end
